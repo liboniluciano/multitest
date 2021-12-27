8355d1c4a89f6c12b3e327afc9121d1c90650215
Luciano Liboni <luciano_liboni@hotmail.com>
2021-12-27 19:20:53 -0300

    refact: removing unsed importations

--- src/api/useCases/FindAllEmployee/index.ts
+++ src/api/useCases/FindAllEmployee/index.ts
@@ -1,9 +1,5 @@
-import { EmployeeRepository } from "../../../repositories/implementations/EmployeeRepository";
-import { CreateLevelUseCase } from "../CreateLevel/CreateLevelUseCase";
 import { FindAllEmployeeController } from "./FindAllEmployeeController";
 import { FindAllEmployeeUseCase } from "./FindAllEmployeeUseCase";
-
-const employeeRepository = new EmployeeRepository();
 
 const findAllEmployeeUseCase = new FindAllEmployeeUseCase();
 
--- src/api/useCases/UpdateEmployee/index.ts
+++ src/api/useCases/UpdateEmployee/index.ts
@@ -1,9 +1,4 @@
-import { FindOneEmployeeUseCase } from "../FindOneEmployee/FindOneEmployeeUseCase";
 import { UpdateEmployeeController } from "./UpdateEmployeeController";
-import { UpdateEmployeeUseCase } from "./UpdateEmployeeUseCase";
-
-const updateEmployeeUseCase = new UpdateEmployeeUseCase();
-const findOneEmployeeUseCase = new FindOneEmployeeUseCase();
 
 const updateEmployeeController = new UpdateEmployeeController();
 
